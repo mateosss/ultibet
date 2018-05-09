e59fe13ba186491912d4b23070b257093c093d4c
mateosss <mateodemayo@gmail.com>
2018-05-09 00:09:12 -0300

    Main menu effects and animations

---
+++ Assets/Scripts/UI/ScrollImage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollImage : MonoBehaviour
+{
+
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+
+}
